b1068596cbacdd9813adb035ea46502b62eee43a
domesama <thethawat.k@mail.kmutt.ac.th>
2020-11-06 18:29:48 +0700

    [Add] React Refs with grids

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,33 @@
-import React from "react";
+//To use React.refs() you'll need a class based component
 
-function ImageCard(props) {
-  const { descriptiom, urls } = props.picture;
-  return (
-    <div>
-      <img src={urls.regular} alt={descriptiom} />
-    </div>
-  );
+import React, { Component } from "react";
+
+export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spans: 0,
+    };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+  render() {
+    const { description, urls } = this.props.picture;
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
+      </div>
+    );
+  }
 }
 
 export default ImageCard;
